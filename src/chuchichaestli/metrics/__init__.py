e95cfc376539b36bc35dd10a17c9f6f779766da4
schlehan <schlehan@students.zhaw.ch>
2025-10-14 14:54:35 +0200

    changing imports for submodule

--- src/chuchichaestli/metrics/__init__.py
+++ src/chuchichaestli/metrics/__init__.py
@@ -3,11 +3,11 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 """Metrics module of chuchichaestli: various loss & evaluation metric implementations."""
 
-from chuchichaestli.metrics.mse import MSE
-from chuchichaestli.metrics.psnr import PSNR
-from chuchichaestli.metrics.ssim import SSIM, SSIMLoss
-from chuchichaestli.metrics.fid import FID
-from chuchichaestli.metrics.lpips import LPIPSLoss
+from chuchichaestli.src.chuchichaestli.metrics.mse import MSE
+from chuchichaestli.src.chuchichaestli.metrics.psnr import PSNR
+from chuchichaestli.src.chuchichaestli.metrics.ssim import SSIM, SSIMLoss
+from chuchichaestli.src.chuchichaestli.metrics.fid import FID
+from chuchichaestli.src.chuchichaestli.metrics.lpips import LPIPSLoss
 
 
 __all__ = ["MSE", "PSNR", "SSIM", "SSIMLoss", "FID", "LPIPSLoss"]
